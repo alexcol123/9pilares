74e3dde43251884567a7042c99dadc9298849ead
Henry Munoz <alexcol123@yahoo.com>
2024-08-06 09:55:58 -0400

    createProfileAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,6 +2,13 @@
 
 import { profileSchema } from './schemas'
 
+import db from './db'
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+
+
 export const testingAction = async (prevState: any,
   formData: FormData) => {
 
@@ -25,13 +32,33 @@ export const createProfileAction = async (
 ) => {
   try {
 
-    const rawData = Object.fromEntries(formData)
+    const user = await currentUser()
+    if (!user) throw new Error('Please login to create a profile')
 
+    const rawData = Object.fromEntries(formData)
     const validatedFields = profileSchema.parse(rawData)
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    })
+
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        tienePerfil: true,
+      },
+    })
+
     // console.log(validatedFields)
     return { message: 'Profile Created' }
   } catch (error) {
     console.log(error)
     return { message: 'there was an error...' }
   }
+
+  redirect('/')
 }
