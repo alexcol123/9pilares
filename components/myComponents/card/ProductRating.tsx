252fac72675f8a4490a336314dfa6b5a3789abea
Henry Munoz <alexcol123@yahoo.com>
2024-08-10 12:02:03 -0400

    chore: Refactor ProductRating component to dynamically generate star ratings and count

--- components/myComponents/card/ProductRating.tsx
+++ components/myComponents/card/ProductRating.tsx
@@ -1,5 +1,10 @@
 
-import { FaStar } from 'react-icons/fa'
+import { count } from "console";
+import { FaStar } from "react-icons/fa6";
+
+import { FaStarHalf } from "react-icons/fa6";
+import { FaRegStar } from "react-icons/fa6";
+
 
 async function ProductRating(
   // { vehicleId, inPage }: { vehicleId: string, inPage: boolean }
@@ -12,18 +17,50 @@ async function ProductRating(
 
   // if (count === 0) { return null }
 
-  let rating = 4.5
-  let count = 100
-  const className = `flex gap-1 items-center  text-xs `
-  const countText = count > 1 ? 'reviews' : 'review'
-  const countValue = `(${count} vendidos) `
+  let count = 10
+  const rating = 3.5
+  let ratingPlaceholder = rating
+
+  const ratingArray = Array.from({ length: 5 }, (_, i) => {
+
+
+    console.log(ratingPlaceholder)
+
+    if (ratingPlaceholder >= 1) {
+      ratingPlaceholder -= 1
+      return 'full'
+    } else if (ratingPlaceholder >= 0.5) {
+      ratingPlaceholder -= 1
+      return 'half'
+    } else {
+      ratingPlaceholder -= 1
+      return 'empty'
+    }
+
+  })
+
+  const starsRatingDiv = <div className="flex">
+    {ratingArray.map((rating, index) => {
+      if (rating === 'full') {
+        return <FaStar key={index} className='w-3 h-3 text-primary' />
+      } else if (rating === 'half') {
+        return <FaStarHalf key={index} className='w-3 h-3 text-primary' />
+      } else {
+        return <FaRegStar key={index} className='w-3 h-3 text-gray-300' />
+      }
+    })}
+
+  </div>
 
   return (
-    <span className={className} >
-      <FaStar className='w-3 h-3' />
-      {rating} {countValue}
-    </span>
+    <div className="flex items-center ">
+      {starsRatingDiv}
+      {count && <span className="text-sm  ml-1">({count})</span>}
+
+    </div>
   )
+
 }
+
 
 export default ProductRating
